fdaa7670f1447c30b071c3249cd56cd2f59d2bc1
Hunter Bertoson <44106297+hkbertoson@users.noreply.github.com>
2024-04-10 16:50:01 -0500

    Netlify Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import {defineConfig} from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'hybrid',
-	adapter: cloudflare(),
+	output: 'server',
+	adapter: netlify(),
 });
