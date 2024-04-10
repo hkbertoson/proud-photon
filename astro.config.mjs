196e191229337432e14c58a82e4d3533c3525b62
Hunter Bertoson <44106297+hkbertoson@users.noreply.github.com>
2024-04-10 16:33:39 -0500

    Various Testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
-import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
+import {defineConfig} from 'astro/config';
 
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  output: "server",
-  adapter: cloudflare()
+	output: 'hybrid',
+	adapter: cloudflare(),
 });
